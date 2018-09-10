2270602c91d9f9df488bc0d7f6c413d2c5997672
Max Leonhardt <m.leonhardt424@gmail.com>
2018-09-10 19:20:42 +0000

    Strlen works, just need to remove last newline

--- printStringLength.s
+++ printStringLength.s
@@ -13,6 +13,22 @@ string:
 main:
     push {lr}
 
+    ldr r0, =inputString
+    bl printf
     
+    ldr r0, =string
+    mov r1, #128
+    ldr r2, =stdin
+    ldr r2, [r2]
+    bl fgets
+
+    ldr r0, =string
+    bl strlen
+    push {r0} /* Push the value from strlen onto the stack */
+
+    ldr r0, =print
+    ldr r1, =string
+    pop {r2}
+    bl printf
 
     pop {pc}
