a5b71a9fca4c7204c909256fd8386255ac0ca09f
glugg23 <m.leonhardt424@gmail.com>
2018-09-14 20:10:32 +0100

    File name can now be chosen

--- cat.s
+++ cat.s
@@ -1,6 +1,15 @@
 .data
+inputMessage:
+    .asciz "Enter file name: "
+
+outputMessage:
+    .asciz "%s"
+
 filename:
-    .asciz "test.txt"
+    .skip 128 @char[128]
+
+line:
+    .skip 512 @char[512]
 
 read:
     .asciz "r"
@@ -10,10 +19,30 @@ read:
 main:
     push {lr}
 
+    ldr r0, =inputMessage
+    bl printf
+
+    ldr r0, =filename
+    mov r1, #128
+    ldr r2, =stdin
+    ldr r2, [r2]
+    bl fgets
+
+    ldr r0, =filename
+    bl strlen
+    sub r0, r0, #1 @Remove one to get right index for adding \0
+
+    mov r1, #0x00
+    ldr r2, =filename
+    str r1, [r2, r0]
+
     ldr r0, =filename
     ldr r1, =read
-    bl fopen
+    bl fopen @fopen(filename, "r")
 
+    mov r4, r0 @save the pointer to filename
+
+    mov r0, r4
     bl fclose
 
     mov r0, #0
