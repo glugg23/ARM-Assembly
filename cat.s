e873202a687b68b4c689f4a75360bc6cbeebc462
Max Leonhardt <m.leonhardt424@gmail.com>
2018-09-18 16:38:01 +0000

    cat now reads in from the command line

--- cat.s
+++ cat.s
@@ -1,12 +1,6 @@
 .data
-inputMessage:
-    .asciz "Enter file name: "
-
 outputMessage:
     .asciz "%s"
-
-filename:
-    .skip 128 @char[128]
 
 line:
     .skip 512 @char[512]
@@ -19,24 +13,7 @@ read:
 main:
     push {lr}
 
-    ldr r0, =inputMessage
-    bl printf
-
-    ldr r0, =filename
-    mov r1, #128
-    ldr r2, =stdin
-    ldr r2, [r2]
-    bl fgets
-
-    ldr r0, =filename
-    bl strlen
-    sub r0, r0, #1 @Remove one to get right index for adding \0
-
-    mov r1, #0x00
-    ldr r2, =filename
-    str r1, [r2, r0]
-
-    ldr r0, =filename
+    ldr r0, [r1, #4] @argv[1] = filename
     ldr r1, =read
     bl fopen @fopen(filename, "r")
 
