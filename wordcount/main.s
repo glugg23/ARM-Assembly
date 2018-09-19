8e201f1642667dcecb6fe6fbd960681a111805de
Max Leonhardt <m.leonhardt424@gmail.com>
2018-09-19 22:25:58 +0000

    Basic loop structure set up for CLA

--- wordcount/main.s
+++ wordcount/main.s
@@ -1,23 +1,34 @@
 .data
-print:
-    .asciz "argc=%d, argv[1]=%s\n"
+input: .asciz "-i"
+output: .asciz "-o"
+inputFilename: .skip 128 @char[128]
+outputFilename: .skip 128
 
 .text
 .global main
 
 commandFlags:
-    push {r11, lr} @Save frame pointer and lr
+    push {fp, lr} @Save frame pointer and lr
     add r11, sp, #0 @Set bottom of stack frame
     sub sp, sp, #16 @Allocate some buffer on stack
 
-    mov r2, r1
-    ldr r2, [r2]
-    mov r1, r0
-    ldr r0, =print
-    bl printf
+    @TODO: Rewrite this using stack?
+    mov r4, r1 @Save argv
+    mov r5, r0 @Save argc
+    mov r6, #0 @i = 0
 
+    loop:
+        cmp r6, r5 @If i >= argc
+        bge end @End loop
+    
+        
+
+        add r6, r6, #1 @i++
+        b loop
+
+    end:
     sub sp, r11, #0 @Readjust stack pointer
-    pop {r11, pc} @Restore frame pointer and pop lr into pc to go back to main
+    pop {fp, pc} @Restore frame pointer and pop lr into pc to go back to main
 
 main:
     bl commandFlags
