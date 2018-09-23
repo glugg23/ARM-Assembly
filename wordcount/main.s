861c437c88d6a1ec00f8100f71fc8c93e3130d75
Max Leonhardt <m.leonhardt424@gmail.com>
2018-09-23 12:50:17 +0000

    Made working out offset easier

--- wordcount/main.s
+++ wordcount/main.s
@@ -14,38 +14,35 @@ commandFlags:
     add fp, sp, #0 @Set bottom of stack frame
     sub sp, sp, #16 @Allocate some buffer on stack
 
-    push {r4, r5, r6} @Save the existing values to ensure we can reset program state
+    push {r4, r5, r6, r7} @Save the existing values to ensure we can reset program state
 
     mov r4, r1 @Save argv
     mov r5, r0 @Save argc
     mov r6, #0 @i = 0
+    mov r7, #0 @Starting offset for argv
 
     loop:
         cmp r6, r5 @If i >= argc
         bge end @End loop
     
-        mov r3, #4
-        mul r3, r6, r3 @Offset for argv
-        push {r3} @Call to strcmp clears r3 so save this value
-                
         ldr r0, =input
-        ldr r1, [r4, r3] @argv[i]
+        ldr r1, [r4, r7] @argv[i]
         bl strcmp @argv[i] == "-i"
 
         cmp r0, #0 @If they are equal
-        pop {r3}
-        add r3, r3, #4 @Increment offset to get next value of argv
-        
-        @Conditionally execute next instructions to save a branch instruction
-        ldreq r0, =inputFilename @Load r0 with pointer to filename
-        ldreq r1, [r4, r3] @Load r1 with argv[i]
-        streq r1, [r0] @Store the value of r1 at *r0
 
+        @Conditionally execute next instructions to save a branch instruction
+        addeq r3, r7, #4 @Increment offset to get next value of argv 
+        ldreq r0, =inputFilename @Load r0 with pointer to filename
+        ldreq r1, [r4, r3] @Load r1 with argv[i + 1]
+        streq r1, [r0] @Store the value of r1 at *r0
+        
         add r6, r6, #1 @i++
+        add r7, r7, #4 @Increase offset by 4 bytes
         b loop
 
     end:
-    pop {r4, r5, r6} @Reset program state
+    pop {r4, r5, r6, r7} @Reset program state
 
     sub sp, fp, #0 @Readjust stack pointer
     pop {fp, pc} @Restore frame pointer and pop lr into pc to go back to main
